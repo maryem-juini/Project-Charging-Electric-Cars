2a567409cd022bd0df96b97e6f176707ce887692
maryem-juini <juinimaryem22@gmail.com>
2024-05-02 13:27:52 +0100

    Developed enter car details and completed my booking page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-import 'package:project/Screens/SearchPage.dart';
+import 'package:project/Screens/EnterCarDetails.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SearchPage(),
+      home: EnterCarDetailsPage(),
     );
   }
 }
