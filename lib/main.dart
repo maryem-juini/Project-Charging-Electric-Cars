edbf6169040b4e13c58e1d23b1ecef57bd6f6ea8
maryem-juini <juinimaryem22@gmail.com>
2024-05-02 01:19:28 +0100

    Booking page and search page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:project/Screens/HomePage.dart';
+import 'package:project/Screens/MyBooking.dart';
+import 'package:project/Screens/SearchPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+      home: BookingPage(),
     );
   }
 }
